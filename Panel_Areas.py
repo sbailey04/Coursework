c01da5b115f3b5222d4cc1bcc74008c43859c19f
sbailey04 <98440944+sbailey04@users.noreply.github.com>
2022-02-11 14:07:54 -0600

    Delete Panel_Areas.py

--- Panel_Areas.py
+++
@@ -1,8 +0,0 @@
-from metpy.plots import declarative
-
-name = input("> Input the postal code area you would like the coordinates for: ")
-
-panel = declarative.MapPanel()
-panel.area = f'{name}'
-
-print(panel.area)
